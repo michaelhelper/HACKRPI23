18c29bf225262d658502a288fd719d580045a1bd
04vmatsibekker <117249183+04vmatsibekker@users.noreply.github.com>
2023-11-04 19:06:24 -0400

    add distance formula

--- maps.js
+++ maps.js
@@ -1,3 +1,19 @@
+
+// Calculate the distance between two sets of coordinates using the Haversine formula.
+function calculateDistance(lat1, lng1, lat2, lng2) {
+    const radius = 6371; // Earth's radius in kilometers
+    const dLat = (lat2 - lat1) * (Math.PI / 180);
+    const dLng = (lng2 - lng1) * (Math.PI / 180);
+    const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(lat1 * (Math.PI / 180)) * Math.cos(lat2 * (Math.PI / 180)) *
+        Math.sin(dLng / 2) * Math.sin(dLng / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = radius * c;
+    return distance;
+}
+
+
 window.onload = function() {
     // get current location
     if (navigator.geolocation) {
