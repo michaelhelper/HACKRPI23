e089e51ce763650fe44e6122d00cf1afb5257dec
04vmatsibekker <117249183+04vmatsibekker@users.noreply.github.com>
2023-11-05 01:25:24 -0400

    Update script.js

--- TinyMCE-OpenAI/script.js
+++ TinyMCE-OpenAI/script.js
@@ -1,16 +1,47 @@
-           tinymce.init({
-                selector: '#editor',
-                plugins: 'ai advtable powerpaste casechange searchreplace autolink advcode image link codesample table  tableofcontents insertdatetime advlist lists checklist wordcount tinymcespellchecker',
-                toolbar: 'undo redo print spellcheckdialog formatpainter | bold italic underline forecolor backcolor | link | alignleft aligncenter alignright alignjustify lineheight | checklist bullist numlist | removeformat | aidialog aishortcuts',
-                height: '700px',
-                ai_shortcuts: [
-                     { title: 'Screenplay', prompt: 'Convert this to screenplay format.' },
-                     { title: 'Stage play', prompt: 'Convert this to stage play format.' },
-                     { title: 'Radio play', prompt: 'Convert this to radio play format.' },
-                     { title: 'Classical', subprompts:
-                        [
-                          { title: 'Dialogue', prompt: 'Convert this to a Socratic dialogue.' },
-                          { title: 'Homeric', prompt: 'Convert this to a Classical Epic.' },
-                        ]
-                      }
-                   ],
+const api_key = "68r83o9poe4v486tnff0u32lv9esr7t53limuhtkbmuxm0n6"
+
+   tinymce.init({
+        selector: 'textarea',
+        ...//content omitted for brevity
+        height: '700px',
+                ai_request: (request, respondWith) => {
+                    const openAiOptions = {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': `Bearer ${api_key}`
+                        },
+                        body: JSON.stringify(
+                            {
+                                model: 'gpt-3.5-turbo',
+                                temperature: 0.7,
+                                max_tokens: 800,
+                                messages: [
+                                    {
+                                        role: 'user',
+                                        content: request.prompt
+                                    }
+                                ]
+                            }
+                        )
+                    };
+
+
+            respondWith.string((signal) => window.fetch('https://api.openai.com/v1/chat/completions', {
+                        signal,
+                        ... openAiOptions
+                    }).then(async (response) => {
+                        if (response.ok) {
+                            return response.json();
+                        } else {
+                            return Promise.reject(`Failed to communicate with the OpenAI API. ${
+                                await response.text()
+                            }`);
+                        }
+                    }).then((data) => data.error ? Promise.reject(`Failed to communicate with the ChatGPT API because of ${
+                        data.error.type
+                    } error: ${
+                        data.error.message
+                    }`) : data).then((data) =>
+                        data ?. choices[0] ?. message ?. content ?. trim()));
+                }
