041384e86507357a9d07681cdab5b53b53563af7
Michael Halpern <100871194+michaelhelper@users.noreply.github.com>
2023-11-05 10:19:02 -0500

    remove v2

--- .history/maps_20231105101843.js
+++ .history/maps_20231105101843.js
@@ -16,7 +16,7 @@ function convertZipCode() {
     fetch(geocodingUrl)
         .then(response => response.json())
         .then(data => {
-            map.removeLayer(marker);
+            map.
             const lat = data.results[0].geometry.location.lat;
             const lng = data.results[0].geometry.location.lng;
             // Set the map view to the lat/long
