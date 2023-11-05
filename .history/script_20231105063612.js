4ebc02bc44df0e5ab9c6382b7642d9942ec0cdda
Michael Halpern <100871194+michaelhelper@users.noreply.github.com>
2023-11-05 08:44:00 -0500

    commented out tiny mc

---
+++ .history/script_20231105063612.js
@@ -0,0 +1,33 @@
+tinymce.init({
+  selector: 'textarea',  // Change this value according to your HTML
+  plugins: 'ai',
+  toolbar: 'aidialog aishortcuts',
+  ai_request: (request, respondWith) => {
+    const openAiOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${env}`
+      },
+      body: JSON.stringify({
+        model: 'gpt-3.5-turbo',
+        temperature: 0.7,
+        max_tokens: 800,
+        messages: [{ role: 'user', content: request.prompt }],
+      })
+    };
+    respondWith.string((signal) => window.fetch('https://api.openai.com/v1/chat/completions', { signal, ...openAiOptions })
+      .then((response) => response.ok ? response.json() : response.text())
+      .then((data) => {
+        if (typeof data === 'string') {
+          Promise.reject(`Failed to communicate with the ChatGPT API.  ${env}`);
+        } else if (data.error) {
+          Promise.reject(`Failed to communicate with the ChatGPT API because of ${data.error.type} error: ${data.error.message}`);
+        } else {
+          // Extract the response content from the data returned by the API
+          return data?.choices[0]?.message?.content?.trim();
+        }
+      })
+    );
+  }
+});
