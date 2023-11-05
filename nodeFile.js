ab63ce3f3eaaa856d4d1da6ebae3b715e78f3818
04vmatsibekker <117249183+04vmatsibekker@users.noreply.github.com>
2023-11-05 05:39:48 -0500

    Update nodeFile.js

--- nodeFile.js
+++ nodeFile.js
@@ -1,9 +1,2 @@
-const apiKey = process.env.MY_API_KEY;
-
-if (!apiKey) {
-  console.error('The MY_API_KEY environment variable is not set.');
-  process.exit(1); // Exit the application with an error code
-}
-
-// Now you can use the apiKey in your code
-console.log(`API Key: ${apiKey}`);
+require("dotenv").config();
+console.log(process.env.OPENAI_API_KEY);
