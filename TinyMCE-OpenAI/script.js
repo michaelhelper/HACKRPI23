8add6c76ee5abf0d88526698bd7b91a2691cf8cd
04vmatsibekker <117249183+04vmatsibekker@users.noreply.github.com>
2023-11-05 00:52:25 -0400

    tinyfirst

---
+++ TinyMCE-OpenAI/script.js
@@ -0,0 +1,6 @@
+ tinymce.init({
+    selector: 'textarea',
+    plugins: 'ai advtable powerpaste casechange searchreplace autolink advcode image link codesample table tableofcontents insertdatetime advlist link lists checklist wordcount tinymcespellchecker',
+    toolbar: 'undo redo print spellcheckdialog formatpainter | bold italic underline forecolor backcolor | link | alignleft aligncenter alignright alignjustify lineheight | checklist bullist numlist | removeformat | aidialog aishortcuts',
+  height: '700px'
+  });
