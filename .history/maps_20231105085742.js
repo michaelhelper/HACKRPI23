3fd872e521070d6e2b0a315c87d63af5e101b0eb
Michael Halpern <100871194+michaelhelper@users.noreply.github.com>
2023-11-05 08:58:49 -0500

    w

--- .history/maps_20231105085742.js
+++ .history/maps_20231105085742.js
@@ -313,7 +313,7 @@ function allcodes(map){
 
 
 
-function afterMapsLoad() {
+function after() {
     theMap = L.map('map').setView([user_lat, user_lng], 11);
     // Get current location
     if (navigator.geolocation) {
