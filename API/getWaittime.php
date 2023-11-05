a7f2470a60fd0e620764511c496bc1ec57a92f79
Toby McDonald <toby_mcdonald@email.com>
2023-11-04 23:46:38 -0400

    Wait Time API

--- API/getWaittime.php
+++ API/getWaittime.php
@@ -1,3 +1,23 @@
 <?php
-die "{\"wait\": \"00:35\"}";
+if (isset($_GET['hosp'])) {
+	$curl = curl_init();
+
+	curl_setopt($curl, CURLOPT_URL, "https://hospitalstats.org/hospital-ratings/".$_GET['hosp'].".htm");
+	//curl_setopt($curl, CURLOPT_USERAGENT, 'Chrome/79');
+	curl_setopt($curl, CURLOPT_USERAGENT, 'HackRPI23/1');
+	curl_setopt($curl, CURLOPT_AUTOREFERER, true);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+	$html = curl_exec($curl);
+	if(curl_error($curl)) {
+		$logger->error('CURL Error', curl_error($curl));
+	}
+	curl_close($curl);
+
+	$out = explode("Time until initial exam: <span class=\"bigstat\">", $html);
+	$out = explode("</span><br/><br/>", $out[1]);
+	die "{\"wait\": \"".$out[0]."\"}";
+}
 ?>
