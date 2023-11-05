894e65ff597ee8266edcc74abe2fcd20e50f5058
04vmatsibekker <117249183+04vmatsibekker@users.noreply.github.com>
2023-11-05 01:37:09 -0500

    alrighty

--- TinyMCE-OpenAI/script.js
+++ TinyMCE-OpenAI/script.js
@@ -1,4 +1,4 @@
-const api_key = "68r83o9poe4v486tnff0u32lv9esr7t53limuhtkbmuxm0n6"
+const api_key = "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"
 
 tinymce.init({
   selector: 'textarea',  // Change this value according to your HTML
