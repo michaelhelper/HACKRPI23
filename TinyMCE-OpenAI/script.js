472c30ea81e23134dae556e6259ae75d85b95ae0
04vmatsibekker <117249183+04vmatsibekker@users.noreply.github.com>
2023-11-05 01:33:39 -0500

    Update script.js

--- TinyMCE-OpenAI/script.js
+++ TinyMCE-OpenAI/script.js
@@ -1,68 +1,35 @@
 const api_key = "68r83o9poe4v486tnff0u32lv9esr7t53limuhtkbmuxm0n6"
- tinymce.init({
-    selector: 'textarea',
-    plugins: 'ai advtable powerpaste casechange searchreplace autolink advcode image link codesample table tableofcontents insertdatetime advlist link lists checklist wordcount tinymcespellchecker',
-    toolbar: 'undo redo print spellcheckdialog formatpainter | bold italic underline forecolor backcolor | link | alignleft aligncenter alignright alignjustify lineheight | checklist bullist numlist | removeformat | aidialog aishortcuts',
-    height: '700px',
 
-
-
-        ai_request: (request, respondWith) => {
-          const openAiOptions = {
-            method: 'POST', 
-            headers: {
-              'Content-Type': 'application/json', 
-              'Authorization': `Bearer ${API_KEY}`
-            }, 
-            body: JSON.stringify({
-                model: 'gpt-3.5-turbo', 
-                temperature: 0.7, 
-                max_tokens: 800, 
-                messages: [
-                  {
-                    role: 'user', 
-                    content: request.prompt
-                  }
-                ]
-            })
-          };
-
-
-
-
-
-
-
-
-           respondWith.string((signal) =>
-                        window.fetch('https://api.openai.com/v1/chat/completions', {
-                            signal, 
-                            ... openAiOptions
-                        }).then(async(response) => {
-                            if (response.ok){
-                            return response.json(); 
-                            } else {
-                                return Promise.reject(`Failed to communicate with the OpenAI API. ${
-                                await response.text()
-                            }`);
-                            }
-                        }).then((data)=> data.error ? 
-                        Promise.reject(`Failed to communicate with ChatGPT API because of ${
-                            data.error.type
-                        } error: ${
-                            data.error.message
-                        }`) : data).then((data) =>
-                            data?.choices[0]?.message?.content?.trim()));
-
-
-
-
-
-
-
-
+tinymce.init({
+  selector: 'textarea',  // Change this value according to your HTML
+  plugins: 'ai',
+  toolbar: 'aidialog aishortcuts',
+  ai_request: (request, respondWith) => {
+    const openAiOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${api_key}`
+      },
+      body: JSON.stringify({
+        model: 'gpt-3.5-turbo',
+        temperature: 0.7,
+        max_tokens: 800,
+        messages: [{ role: 'user', content: request.prompt }],
+      })
+    };
+    respondWith.string((signal) => window.fetch('https://api.openai.com/v1/chat/completions', { signal, ...openAiOptions })
+      .then((response) => response.ok ? response.json() : response.text())
+      .then((data) => {
+        if (typeof data === 'string') {
+          Promise.reject(`Failed to communicate with the ChatGPT API. ${data}`);
+        } else if (data.error) {
+          Promise.reject(`Failed to communicate with the ChatGPT API because of ${data.error.type} error: ${data.error.message}`);
+        } else {
+          // Extract the response content from the data returned by the API
+          return data?.choices[0]?.message?.content?.trim();
         }
-
-
-
-  });
+      })
+    );
+  }
+});
