0817f8fdd6a4d361e453254c15ced60a8eb1acbc
EdenBendheim <edenbendheim@gmail.com>
2023-11-05 06:31:23 -0500

    attemt to make enter work

--- maps.js
+++ maps.js
@@ -327,6 +327,13 @@ window.onload = function() {
     //call allcodes
     allcodes(map);
 }
+
+function clickPress(event) {
+    if (event.keyCode == 13) {
+        zipcode();
+    }
+}
+
 function zipcode() {
     // Get the ZIP code from the form
     const zipCode = document.getElementById('search-input').value;
